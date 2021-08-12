dc6fcfb98f60be1c4ea09980cef5ffa4b08db6db
Slaysenfite <wessels.sm@gmail.com>
2021-08-12 19:59:47 +0200

    Fix aug, update main scripts

--- python_src/GoogLeNetMain.py
+++ python_src/GoogLeNetMain.py
@@ -1,26 +1,28 @@
 import sys
+import time
+from datetime import timedelta
 
 import tensorflow as tf
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from tensorflow.python.keras.applications.inception_v3 import InceptionV3
-from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.utils import to_categorical
 
-from configurations.DataSet import binary_ddsm_data_set as data_set
-from configurations.TrainingConfig import IMAGE_DIMS, create_required_directories, hyperparameters, create_callbacks
+from configurations.DataSet import bcs_data_set as data_set
+from configurations.TrainingConfig import IMAGE_DIMS, create_required_directories, hyperparameters, create_callbacks, \
+    MODEL_OUTPUT
 from metrics.MetricsReporter import MetricReporter
-from networks.NetworkHelper import compile_with_regularization, create_classification_layers
-from utils.Emailer import results_dispatch
+from networks.NetworkHelper import create_classification_layers, compile_with_regularization
+from training_loops.CustomCallbacks import RunMetaHeuristicOnPlateau
+from training_loops.CustomTrainingLoop import training_loop
 from utils.ImageLoader import load_rgb_images, supplement_training_data
-from utils.ScriptHelper import generate_script_report, read_cmd_line_args
+from utils.ScriptHelper import generate_script_report, read_cmd_line_args, create_file_title
 
 print('Python version: {}'.format(sys.version))
 print('Tensorflow version: {}\n'.format(tf.__version__))
 print('[BEGIN] Start script...\n')
-read_cmd_line_args(data_set, hyperparameters, IMAGE_DIMS)
+hyperparameters, opt, data_set = read_cmd_line_args(hyperparameters, data_set)
+print(' Dataset: {}\n'.format(data_set.name))
 print(' Image dimensions: {}\n'.format(IMAGE_DIMS))
 print(hyperparameters.report_hyperparameters())
 
@@ -36,71 +38,97 @@ data, labels = load_rgb_images(data, labels, data_set, IMAGE_DIMS)
 
 # partition the data into training and testing splits using 70% of
 # the data for training and the remaining 30% for testing
-(train_x, test_x, train_y, test_y) = train_test_split(data, labels, test_size=0.3, train_size=0.7, random_state=42)
+(train_x, test_x, train_y, test_y) = train_test_split(data, labels, test_size=0.2, train_size=0.8, random_state=42)
 
-print('[INFO] Augmenting data set')
-aug = ImageDataGenerator(
-    horizontal_flip=True,
-    vertical_flip=True,
-    rotation_range=10,
-    zoom_range=0.05,
-    fill_mode="nearest")
 
-train_x, train_y = supplement_training_data(aug, train_x, train_y)
+if hyperparameters.augmentation:
+    print('[INFO] Augmenting data set')
+    aug = ImageDataGenerator(
+        horizontal_flip=True,
+        vertical_flip=True,
+        rotation_range=10,
+        zoom_range=0.05,
+        fill_mode='nearest')
 
-print("[INFO] Training data shape: " + str(train_x.shape))
-print("[INFO] Training label shape: " + str(train_y.shape))
+    train_x, train_y = supplement_training_data(aug, train_x, train_y, multiclass=False)
 
-if data_set.is_multiclass:
-    print('[INFO] Configure for multiclass classification')
-    lb = LabelBinarizer()
-    train_y = lb.fit_transform(train_y)
-    test_y = lb.transform(test_y)
-    loss = 'categorical_crossentropy'
+print('[INFO] Training data shape: ' + str(train_x.shape))
+print('[INFO] Training label shape: ' + str(train_y.shape))
+
+loss, train_y, test_y = data_set.get_dataset_labels(train_y, test_y)
+
+if hyperparameters.preloaded_weights:
+    print('[INFO] Loading imagenet weights')
+    weights = 'imagenet'
 else:
-    print('[INFO] Configure for binary classification')
-    train_y = to_categorical(train_y)
-    test_y = to_categorical(test_y)
-    loss = 'binary_crossentropy'
+    weights = None
+model = InceptionV3(
+        include_top=False,
+        weights=weights,
+        input_shape=IMAGE_DIMS,
+        classes=len(data_set.class_names))
+model = create_classification_layers(base_model=model, classes=len(data_set.class_names))
 
-print('[INFO] Adding callbacks')
-callbacks = create_callbacks()
+if hyperparameters.weights_of_experiment_id is not None:
+    path_to_weights = '{}{}.h5'.format(MODEL_OUTPUT, hyperparameters.weights_of_experiment_id)
+    print('[INFO] Loading weights from {}'.format(path_to_weights))
+    model.load_weights(path_to_weights)
 
-model = InceptionV3(input_shape=IMAGE_DIMS, classes=len(data_set.class_names), weights=None, include_top=True)
+# Compile model
+compile_with_regularization(model=model,
+                            loss='binary_crossentropy',
+                            optimizer=opt,
+                            metrics=['accuracy'],
+                            regularization_type='l2')
 
-opt = SGD(learning_rate=hyperparameters.init_lr, decay=True)
-compile_with_regularization(model, loss=loss, optimizer=opt, metrics=['accuracy'],
-                            regularization_type='l2', attrs=['weight_regularizer'], l2=0.005)
+# Setup callbacks
+callbacks = create_callbacks(hyperparameters)
+
+if hyperparameters.meta_heuristic != 'none':
+    meta_callback = RunMetaHeuristicOnPlateau(
+        X=train_x, y=train_y, meta_heuristic=hyperparameters.meta_heuristic, population_size=25, iterations=10,
+monitor='val_loss', factor=0.2, patience=4, verbose=1, mode='min',
+        min_delta=0.05, cooldown=0)
+    callbacks.append(meta_callback)
 
 # train the network
-H = model.fit(x=aug.flow(train_x, train_y, batch_size=hyperparameters.batch_size), validation_data=(test_x, test_y),
-              steps_per_epoch=len(train_x) // hyperparameters.batch_size, epochs=hyperparameters.epochs,
-              callbacks=callbacks)
+start_time = time.time()
+
+if hyperparameters.tf_fit:
+    H = model.fit(train_x, train_y, batch_size=hyperparameters.batch_size, validation_data=(test_x, test_y),
+                  steps_per_epoch=len(train_x) // hyperparameters.batch_size, epochs=hyperparameters.epochs,
+                  callbacks=callbacks)
+else:
+    H = training_loop(model, opt, hyperparameters, train_x, train_y, test_x, test_y,
+                      meta_heuristic=hyperparameters.meta_heuristic,
+                      meta_heuristic_order=hyperparameters.meta_heuristic_order)
+
+time_taken = timedelta(seconds=(time.time() - start_time))
 
 # evaluate the network
 print('[INFO] evaluating network...')
 
-predictions = model.predict(test_x, batch_size=hyperparameters.batch_size)
+acc = model.evaluate(test_x, test_y)
+print(str(model.metrics_names))
+print(str(acc))
+
+predictions = model.predict(test_x)
 
 print('[INFO] generating metrics...')
 
-generate_script_report(H, test_y, predictions, data_set, hyperparameters, 'googlenet')
+file_title = create_file_title('GoogLeNet', hyperparameters)
 
-reporter = MetricReporter(data_set.name, 'googlenet')
+model.save(filepath=MODEL_OUTPUT + file_title + '.h5', save_format='h5')
+
+generate_script_report(H, model, test_x, test_y, predictions, time_taken, data_set, hyperparameters, file_title)
+
+reporter = MetricReporter(data_set.name,file_title)
 cm1 = confusion_matrix(test_y.argmax(axis=1), predictions.argmax(axis=1))
 reporter.plot_confusion_matrix(cm1, classes=data_set.class_names,
                                title='Confusion matrix, without normalization')
 
 reporter.plot_roc(data_set.class_names, test_y, predictions)
 
-reporter.plot_network_metrics(H, 'googlenet')
-
-# print('[INFO] serializing network and label binarizer...')
-#
-# reporter.save_model_to_file(model, lb)
-
-print('[INFO] emailing result...')
-
-results_dispatch(data_set.name, 'googlenet')
+reporter.plot_network_metrics(H, file_title)
 
 print('[END] Finishing script...\n')
