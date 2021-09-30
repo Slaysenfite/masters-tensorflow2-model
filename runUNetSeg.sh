4db8db046e380c326245e89e02ccd1ce5e667c05
Slaysenfite <wessels.sm@gmail.com>
2021-09-30 10:37:52 +0200

    Amend UnetSeg Script

--- runUNetSeg.sh
+++ runUNetSeg.sh
@@ -1,4 +1,2 @@
-python python_src/UNetSegMain.py --id=MSUD001 --dataset=cbis_seg --optimizer=sgd --preloaded_weights=False --epochs=100
-python python_src/UNetSegMain.py --id=MSUD001-ImageNet --dataset=cbis_seg --optimizer=sgd --preloaded_weights=True --epochs=100
-python python_src/UNetSegMain.py --id=MSUD001Pso --preloaded_experiment=MSUD001-ImageNet --dataset=cbis_seg --optimizer=sgd --epochs=100 --meta_heuristic=pso
-python python_src/UNetSegMain.py --id=MSUD001Ga --preloaded_experiment=MSUD001-ImageNet --dataset=cbis_seg --optimizer=sgd --epochs=100 --meta_heuristic=ga
+python python_src/UNetSegMain.py --id=MSUD001Pso --preloaded_experiment=MSUD001-ImageNet --dataset=cbis_seg --optimizer=sgd --epochs=100 --meta_heuristic=pso --preloaded_weights=True
+python python_src/UNetSegMain.py --id=MSUD001Ga --preloaded_experiment=MSUD001-ImageNet --dataset=cbis_seg --optimizer=sgd --epochs=100 --meta_heuristic=ga --preloaded_weights=True
