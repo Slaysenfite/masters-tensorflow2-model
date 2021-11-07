5b3b6288b67cf3182347c31d4b5ec89267f8a311
Slaysenfite <wessels.sm@gmail.com>
2021-11-07 09:53:56 +0200

    Blah

--- python_src/NewXceptionNetMain.py
+++ python_src/NewXceptionNetMain.py
@@ -4,15 +4,18 @@ import time
 from datetime import timedelta
 
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix
 from tensorflow.python.keras.applications.xception import Xception
 
 from configurations.DataSet import cbis_ddsm_data_set as data_set
 from configurations.TrainingConfig import IMAGE_DIMS, create_required_directories, hyperparameters, create_callbacks, \
     MODEL_OUTPUT
 from networks.NetworkHelper import create_classification_layers, compile_with_regularization, generate_heatmap
+from python_src.metrics.MetricsReporter import MetricReporter
 from training_loops.CustomCallbacks import RunMetaHeuristicOnPlateau
 from training_loops.CustomTrainingLoop import training_loop
-from utils.ScriptHelper import read_cmd_line_args, evaluate_classification_model, evaluate_meta_model
+from utils.ScriptHelper import read_cmd_line_args, evaluate_classification_model, evaluate_meta_model, \
+    create_file_title, generate_script_report
 
 print('Python version: {}'.format(sys.version))
 print('Tensorflow version: {}\n'.format(tf.__version__))
@@ -53,11 +56,60 @@ if hyperparameters.weights_of_experiment_id is not None:
     print('[INFO] Loading weights from {}'.format(path_to_weights))
     model.load_weights(path_to_weights)
 
-# compile model
-    model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
+# Compile model
+compile_with_regularization(model=model,
+                            loss='binary_crossentropy',
+                            optimizer=opt,
+                            metrics=['accuracy'],
+                            regularization_type='l2',
+                            l2=hyperparameters.l2)
+
+# Setup callbacks
+callbacks = create_callbacks(hyperparameters)
+
+if hyperparameters.meta_heuristic != 'none':
+    meta_callback = RunMetaHeuristicOnPlateau(
+        X=train_x, y=train_y, meta_heuristic=hyperparameters.meta_heuristic, population_size=30, iterations=10,
+        monitor='val_loss', patience=4, verbose=1, mode='min',
+        min_delta=0.05, cooldown=0)
+    callbacks.append(meta_callback)
 
 # train the network
 start_time = time.time()
+
+if hyperparameters.tf_fit:
+    H = model.fit(train_x, train_y, batch_size=hyperparameters.batch_size, validation_data=(test_x, test_y),
+                  steps_per_epoch=len(train_x) // hyperparameters.batch_size, epochs=hyperparameters.epochs,
+                  callbacks=callbacks)
+else:
+    H = training_loop(model, opt, hyperparameters, train_x, train_y, test_x, test_y,
+                      meta_heuristic=hyperparameters.meta_heuristic)
+
+time_taken = timedelta(seconds=(time.time() - start_time))
+
+# evaluate the network
+print('[INFO] evaluating network...')
+
+acc = model.evaluate(test_x, test_y)
+print(str(model.metrics_names))
+print(str(acc))
+
+predictions = model.predict(test_x)
+
+print('[INFO] generating metrics...')
+
+file_title = create_file_title('Xception', hyperparameters)
+
+generate_script_report(H, model, test_x, test_y, predictions, time_taken, data_set, hyperparameters, file_title)
+
+reporter = MetricReporter(data_set.name, file_title)
+cm1 = confusion_matrix(test_y.argmax(axis=1), predictions.argmax(axis=1))
+reporter.plot_confusion_matrix(cm1, classes=data_set.class_names,
+                               title='Confusion matrix, without normalization')
+
+reporter.plot_roc(data_set.class_names, test_y, predictions)
+
+reporter.plot_network_metrics(H, file_title)
 
 print('META-HEURISTIC')
 
