99f7bf65af42f306a3557f80025f4a128755cf22
Slaysenfite <wessels.sm@gmail.com>
2021-11-12 11:48:21 +0200

    Change seg meta OF to bad dice

--- python_src/training_loops/OptimizerHelper.py
+++ python_src/training_loops/OptimizerHelper.py
@@ -7,7 +7,7 @@ from tensorflow.python.keras.metrics import TrueNegatives, TruePositives, FalseP
 from tensorflow.python.ops.numpy_ops.np_arrays import convert_to_tensor
 
 from metrics.LossFunctions import SegmentationLossFunctions
-from metrics.MetricsUtil import iou_coef
+from metrics.MetricsUtil import iou_coef, dice_coef
 
 MAX_LAYERS_FOR_OPTIMIZATION = 6
 
@@ -45,8 +45,8 @@ def calc_seg_fitness(weights, model, loss_metric, X, y, num_layers):
     set_trainable_weights(model=model, weights=weights, num_layers=num_layers)
     predictions = model.predict(X)
     ŷ = convert_to_tensor(predictions)
-    s = SegmentationLossFunctions()
-    return 1 - s.sensitivity(y, ŷ)
+
+    return 1 - dice_coef(y, ŷ)
 
 
 def calc_solution_fitness_only_loss(weights, model, loss_metric, X, y, num_layers):
